4c838bf6ebcf3903e22d05f00ad8e6a448bfb13d
Christian Gregg <christian@bissy.io>
2021-12-17 13:18:34 +0000

    Update profile

--- setup.sh
+++ setup.sh
@@ -3,17 +3,29 @@
 set -euxo pipefail
 
 echo "Logging you into Azure..."
+echo "Press any key to continue."
+read -n 1
 az login -o json > azure_subscriptions.json
 az ad signed-in-user show | jq -r .mail > azure_email.txt
 az account list-locations > azure_locations.json
 
 echo "Logging you into GitHub..."
+echo "Press any key to continue."
+read -n 1
+
 gh auth login
 gh api "/" --jq .current_user_url | awk -F[/:] '{print $4}' > github_host.txt
 gh api "/user/memberships/orgs?state=active" > github_orgs.json
 
+echo "Starting the arc-setup script"
+echo "Press any key to continue."
+read -n 1
+
 arc-setup
 
+echo "Terraforming - This will provision new resources to Azure..."
+echo "Press any key to continue."
+read -n 1
 terraform apply -target azurerm_kubernetes_cluster.arc
 terraform apply \
   -target helm_release.ingress \
