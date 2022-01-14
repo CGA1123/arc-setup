07b900d0fdd83d3141cee07b21a9d0caa7eedd32
Christian Gregg <christian@bissy.io>
2022-01-14 12:52:05 +0000

    Add sample workflow

--- script/install.sh
+++ script/install.sh
@@ -3,3 +3,8 @@
 ./script/bootstrap.sh
 ./script/start.sh
 ./script/configure.sh
+
+echo "ℹ Actions Runner Controller is now installed!"
+echo "The following workflow file can be used in order to test it out:"
+
+./script/subst.sh ./data/workflow.yml
